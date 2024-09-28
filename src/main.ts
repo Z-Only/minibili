9652805b299bb2ad66439444b688766ac0bcd8df
zhao <m2_zhao@foxmail.com>
2024-09-28 14:00:38 +0800

    feat(vuetify): add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { md3 } from "vuetify/blueprints";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+import { zhHans, en } from "vuetify/locale";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(router)
+  .use(
+    createVuetify({
+      blueprint: md3,
+      icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+          mdi,
+        },
+      },
+      locale: {
+        locale: "zhHans",
+        fallback: "en",
+        messages: { zhHans, en },
+      },
+      theme: {
+        defaultTheme: "light",
+      },
+    })
+  )
+  .mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import vuetify from "vite-plugin-vuetify";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [vue(), vuetify()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
