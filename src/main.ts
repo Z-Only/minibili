2e6557ac9f244fb4f38f414f430b1c56e8850983
zhao <m2_zhao@foxmail.com>
2024-09-28 23:17:47 +0800

    fix(theme): import vuetify theme css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import 'vuetify/styles';
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from "vuetify";
 import { md3 } from "vuetify/blueprints";
