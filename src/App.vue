cc451f4d39aea885b89db01c8281ce7089bafac1
zhao <m2_zhao@foxmail.com>
2024-09-29 00:14:27 +0800

    feat(route): modify route history mode from memory to html5

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 const theme = ref("light");
+
+const router = useRouter();
 
 const onClick = () => {
   theme.value = theme.value === "light" ? "dark" : "light";
@@ -9,13 +12,30 @@ const onClick = () => {
 </script>
 
 <template>
-  <v-responsive class="border rounded" max-height="300">
+  <v-responsive class="border rounded" max-height="3000">
     <v-app :theme="theme">
       <v-app-bar title="MiniBili" class="px-3">
+        <v-icon
+          icon="mdi-arrow-left"
+          :disabled="router.options.history.state.back != null"
+          @click.prevent="router.back()"
+        ></v-icon>
+        <v-icon
+          icon="mdi-arrow-right"
+          :disabled="router.options.history.state.forward != null"
+          @click.prevent="router.forward()"
+        ></v-icon>
+        <v-icon icon="mdi-refresh" @click.prevent="router.go(0)"></v-icon>
+        <v-icon
+          icon="mdi-home-outline"
+          @click.prevent="router.push({ name: 'Home' })"
+        ></v-icon>
         <v-spacer></v-spacer>
 
         <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          :prepend-icon="
+            theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
+          "
           text="Toggle Theme"
           slim
           @click="onClick"
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,22 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/components/Home.vue";
 
 const routes = [
   {
     path: "/",
+    name: "Home",
     component: Home,
   },
   { path: "/home", redirect: "/" },
   {
     path: "/setting",
+    name: "Setting",
     component: () => import("@/components/Setting.vue"),
   },
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
 
