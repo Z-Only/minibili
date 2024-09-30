f48b881b5a331de748f0818940a28dfce4381e21
zhao <m2_zhao@foxmail.com>
2024-09-30 23:10:53 +0800

    chore(eslint): update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,9 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
     { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+    {
+        ignores: ['dist/**', 'src/vite-env.d.ts', 'src-tauri/**'],
+    },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
@@ -13,6 +16,12 @@ export default [
     {
         files: ['**/*.vue'],
         languageOptions: { parserOptions: { parser: tseslint.parser } },
+    },
+    {
+        rules: {
+            'vue/multi-word-component-names': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+        },
     },
     eslintConfigPrettier,
 ]
