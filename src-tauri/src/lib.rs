561683608249d8096d64bb827de03d38193c7285
zhao <m2_zhao@foxmail.com>
2024-10-09 02:55:00 +0800

    refactor(plugin): install tauri clipboard-manager plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ mod video;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(
             tauri_plugin_log::Builder::new()
                 .targets([
