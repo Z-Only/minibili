6caa005f469e1abe9b98292478a911cf65be8a29
zhao <m2_zhao@foxmail.com>
2024-10-18 02:32:41 +0800

    feat(tray): add tooltip and more menu items for system tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 use tauri::{
     menu::{Menu, MenuItem},
-    tray::TrayIconBuilder,
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    Manager,
 };
 use tauri_plugin_log::{Target, TargetKind};
 
@@ -27,15 +28,44 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![commands::fetch])
         .setup(|app| {
             // create a menu
+            let open_i = MenuItem::with_id(app, "open", "Open", true, None::<&str>)?;
+            let hide_i = MenuItem::with_id(app, "hide", "Hide", true, None::<&str>)?;
             let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&quit_i])?;
-            // create a tray icon
+            let menu = Menu::with_items(app, &[&open_i, &hide_i, &quit_i])?;
+
             let _tray = TrayIconBuilder::new()
+                .on_tray_icon_event(|tray, event| match event {
+                    TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } => {
+                        println!("left click pressed and released");
+                        // in this example, let's show and focus the main window when the tray is clicked
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    _ => {}
+                })
+                // create a tray icon
                 .icon(app.default_window_icon().unwrap().clone())
+                // set the tooltip
+                .tooltip("My App")
                 .menu(&menu)
                 .menu_on_left_click(false)
                 // handle menu events
                 .on_menu_event(|app, event| match event.id.as_ref() {
+                    "open" => {
+                        println!("open menu item was clicked");
+                        app.get_webview_window("main").unwrap().show().unwrap();
+                    }
+                    "hide" => {
+                        println!("hide menu item was clicked");
+                        app.get_webview_window("main").unwrap().hide().unwrap();
+                    }
                     "quit" => {
                         println!("quit menu item was clicked");
                         app.exit(0);
