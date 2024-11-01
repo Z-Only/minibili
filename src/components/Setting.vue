ba684099462724f01d034cfd8b53fc435e3a26cf
vv <169345556+chanyu-vv@users.noreply.github.com>
2024-11-01 11:59:11 +0800

    build(config): add tauri configurations for multiple platforms

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -3,36 +3,39 @@ import { openDevTools } from '@/common/commands'
 </script>
 
 <template>
-    <v-card class="mx-auto" max-width="600">
-        <v-list>
-            <v-list-subheader>系统设置</v-list-subheader>
+    <v-container class="d-flex flex-column justify-start align-center">
+        <v-img width="200" cover src="/icon.png" alt="LOGO"></v-img>
+        <v-card class="mx-auto" width="600">
+            <v-list>
+                <v-list-subheader>系统设置</v-list-subheader>
 
-            <v-list-item title="开发者工具">
-                <template v-slot:append>
-                    <v-btn
-                        icon="mdi-greater-than"
-                        variant="text"
-                        @click.prevent="openDevTools"
-                    ></v-btn>
-                </template>
-            </v-list-item>
+                <v-list-item title="开发者工具">
+                    <template v-slot:append>
+                        <v-btn
+                            icon="mdi-greater-than"
+                            variant="text"
+                            @click.prevent="openDevTools"
+                        ></v-btn>
+                    </template>
+                </v-list-item>
 
-            <v-divider></v-divider>
+                <v-divider></v-divider>
 
-            <v-list-subheader>其它设置</v-list-subheader>
+                <v-list-subheader>其它设置</v-list-subheader>
 
-            <v-list-item title="用户协议">
-                <template v-slot:append>
-                    <v-btn icon="mdi-greater-than" variant="text"></v-btn>
-                </template>
-            </v-list-item>
-            <v-list-item title="隐私政策">
-                <template v-slot:append>
-                    <v-btn icon="mdi-greater-than" variant="text"></v-btn>
-                </template>
-            </v-list-item>
-        </v-list>
-    </v-card>
+                <v-list-item title="用户协议">
+                    <template v-slot:append>
+                        <v-btn icon="mdi-greater-than" variant="text"></v-btn>
+                    </template>
+                </v-list-item>
+                <v-list-item title="隐私政策">
+                    <template v-slot:append>
+                        <v-btn icon="mdi-greater-than" variant="text"></v-btn>
+                    </template>
+                </v-list-item>
+            </v-list>
+        </v-card>
+    </v-container>
 </template>
 
 <style scoped></style>
